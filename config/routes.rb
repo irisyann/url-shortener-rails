f2dd80486c057c9d44fe1862d3c0f06d2c9a55f9
irisyann <irisyan716@gmail.com>
2023-03-22 15:09:19 +0800

    move route sequence in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "main#index"
-
-  resources :url_creations, only: [:new, :create] do
-    resources :short_urls, only: [:new, :create] do
-      resources :geolocations, only: [:new, :create]
-    end
-  end
-
-  get "usage_report", to: "usage_reports#index"
-
-  get "/:short_path", to: "url_creations#redirect"
-
   ######## Authentication Routes ########
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
@@ -31,6 +18,19 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+
+  ######## Application Routes ########
+  root "main#index"
+
+  resources :url_creations, only: [:new, :create] do
+    resources :short_urls, only: [:new, :create] do
+      resources :geolocations, only: [:new, :create]
+    end
+  end
+
+  get "usage_report", to: "usage_reports#index"
+
+  get "/:short_path", to: "url_creations#redirect"
   
 
 end
