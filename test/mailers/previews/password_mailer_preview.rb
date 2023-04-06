fb75c19bef07fd06a870b8d137fb5e64988e6b08
Iris Y <irisyan716@gmail.com>
2023-04-06 16:51:43 +0800

    add padding to signup and signin buttons on index

--- test/mailers/previews/password_mailer_preview.rb
+++ test/mailers/previews/password_mailer_preview.rb
@@ -1,7 +1,7 @@
-# Preview all emails at http://localhost:3000/rails/mailers/password_mailer
+# Preview all emails at http://localhost:3001/rails/mailers/password_mailer
 class PasswordMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/password_mailer/reset
+  # Preview this email at http://localhost:3001/rails/mailers/password_mailer/reset
   def reset
     PasswordMailer.reset
   end
